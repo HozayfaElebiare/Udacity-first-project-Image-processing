c364d2db932a5e30906aa93b651a0e44532cdbb2
Hozayfa Elebiare <Admin@programmingmap.com>
2022-01-21 21:47:00 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express, { Application, Request, Response } from 'express'
+import morgan from 'morgan'
+import * as dotenv from 'dotenv'
+dotenv.config()
+const PORT = process.env.PORT || 3000
+
+// express create an instance server 
+const app: Application = express()
+
+//morgan  HTTP request logger middleware
+app.use(morgan('dev'))
+
+//main api with / path
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    Project : 'Udacity first project',
+    projectName : 'Image processing',
+    author : 'Mahmoud Elebiare',
+    authorEmail: 'mahmoudelebiare@gmail.com'
+  })
+})
+
+// listin port and start express server
+app.listen(PORT, () => {
+  console.log(`Server is starting at prot: http://localhost:${PORT}`)
+})
+
+export default app
+ 
